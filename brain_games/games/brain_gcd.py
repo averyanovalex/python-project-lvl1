52fef09fcb4d911d51824d5a898c79c47992caf1
Alexey Averyanov <averyanovalexey@gmail.com>
2022-01-11 09:41:20 +0600

    add gcd game

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,65 @@
+"""Greatest Common Devizor game."""
+
+from brain_games.cli import answer_int, print_text, welcome_user
+from brain_games.games.common import (
+    ROUNDS,
+    check_answer,
+    congratulate_user,
+    random_int,
+)
+
+
+def calc_gcd(num1: int, num2: int):
+    """
+    Calulate greatest common devizor.
+
+    Args:
+        num1: first number
+        num2: second number
+
+    Returns:
+        int
+    """
+    min_num = min(num1, num2)
+    gcd = 1
+    for candidate in range(2, min_num + 1):
+        if num1 % candidate == 0 and num2 % candidate == 0:
+            gcd = candidate
+
+    return gcd
+
+
+def prepare_question_and_answer():
+    """
+    Prepare question and correct answer for user.
+
+    Generate 2 numbers , calculate answer.
+    Prepare question and correct answer as strings.
+
+    Returns:
+        str, str
+    """
+    num1 = random_int()
+    num2 = random_int()
+
+    question = 'Question: {s1} {s2}'.format(s1=str(num1), s2=str(num2))
+    right_answer = str(calc_gcd(num1, num2))
+
+    return question, right_answer
+
+
+def brain_gcd():
+    """Implementaion of game's logic."""
+    name = welcome_user()
+    print_text('Find the greatest common divisor of given numbers.')
+
+    for _ in range(ROUNDS):
+        question, right_answer = prepare_question_and_answer()
+
+        answer = str(answer_int(question))
+        is_correct = check_answer(answer, right_answer, name)
+
+        if not is_correct:
+            return
+
+    congratulate_user(name)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Game 'Greatest common devizor'  entrypoint."""
+
+from brain_games.games.brain_gcd import brain_gcd
+
+
+def main():
+    """Entrypoint function."""
+    brain_gcd()
+
+
+if __name__ == '__main__':
+    main()
