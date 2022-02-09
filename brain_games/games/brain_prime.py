07697e3b1974bc56cfa27ef91ed9c6659b3842ad
Alexey Averyanov <averyanovalexey@gmail.com>
2022-02-09 23:19:08 +0600

    insert calling run_game in brain-gcd, brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,13 +1,34 @@
 """Prime number game."""
 
-from brain_games.cli import answer_yes_no, print_text, welcome_user
-from brain_games.games.common import (
-    ROUNDS,
-    calc_gcd,
-    check_answer,
-    congratulate_user,
-    random_int,
-)
+from brain_games.cli import answer_yes_no
+from brain_games.games.common import calc_gcd, random_int, run_game
+
+
+def brain_prime() -> None:
+    """Implementaion of game's logic."""
+    welcome_message = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    build_question = prepare_question_and_answer
+    ask_question = answer_yes_no
+
+    run_game(welcome_message, build_question, ask_question)
+
+
+def prepare_question_and_answer() -> tuple:
+    """
+    Prepare question and correct answer for user.
+
+    Generate number , calculate answer.
+    Prepare question and correct answer as strings.
+
+    Returns:
+        str, str
+    """
+    num = random_int()
+
+    question = 'Question: {s1}'.format(s1=str(num))
+    right_answer = 'yes' if is_prime(num) else 'no'
+
+    return question, right_answer
 
 
 def is_prime(number: int) -> bool:
@@ -29,38 +50,3 @@ def is_prime(number: int) -> bool:
             return False
 
     return True
-
-
-def prepare_question_and_answer() -> tuple:
-    """
-    Prepare question and correct answer for user.
-
-    Generate number , calculate answer.
-    Prepare question and correct answer as strings.
-
-    Returns:
-        str, str
-    """
-    num = random_int()
-
-    question = 'Question: {s1}'.format(s1=str(num))
-    right_answer = 'yes' if is_prime(num) else 'no'
-
-    return question, right_answer
-
-
-def brain_prime() -> None:
-    """Implementaion of game's logic."""
-    name = welcome_user()
-    print_text('Answer "yes" if given number is prime. Otherwise answer "no".')
-
-    for _ in range(ROUNDS):
-        question, right_answer = prepare_question_and_answer()
-
-        answer = answer_yes_no(question)
-        is_correct = check_answer(answer, right_answer, name)
-
-        if not is_correct:
-            return
-
-    congratulate_user(name)
