070640a7228c47e82b5efbd87e8c7de5dd991b44
Alexey Averyanov <averyanovalexey@gmail.com>
2022-02-25 22:26:43 +0600

    fix brain-prime (step 6)

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -38,4 +38,5 @@ def ask(question: str) -> str:
 
     """
     print_text('Question: {0}'.format(question))
-    return prompt.string('Your answer: ', empty=True)
+    answer = prompt.string('Your answer: ', empty=True)
+    return '' if answer is None else answer
